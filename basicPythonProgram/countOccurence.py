7092e1dc2d41e0b52a308c1ed319c658f66f3e70
sandeep786 <sadeeprajsingh05@gmail.com>
2020-11-04 09:58:50 +0530

    ADDED: Feature to count number of occurence

---
+++ basicPythonProgram/countOccurence.py
@@ -0,0 +1,15 @@
+"""
+author - sandeep raj
+date - 04/11/2020
+time - 09:50
+problem statement -"Write a Python program to count the number occurrence of a specific character in a string."
+"""
+
+
+given_string = "my name is sandeep raj"
+try:
+ print()
+ print(given_string.count("s"))
+ print()
+except:
+    print("Provide Proper String And Checked  InBuilt Function")
