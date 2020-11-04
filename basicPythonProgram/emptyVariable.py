20412dfe1c6dd9d937aae714f8e2adea7ab77107
sandeep786 <sadeeprajsingh05@gmail.com>
2020-11-04 21:46:38 +0530

    ADDED: Feature tp empty a varibale without destroy

---
+++ basicPythonProgram/emptyVariable.py
@@ -0,0 +1,15 @@
+"""
+author - sandeep raj
+date - 04/11/2020
+time - 21:32
+problem statement -"Write a Python program to empty a variable without destroying it."
+"""
+
+n = 20
+d = {"x":200}
+l = [1,3,5]
+t= (5,7,8)
+print(type(n)())
+print(type(d)())
+print(type(l)())
+print(type(t)())
