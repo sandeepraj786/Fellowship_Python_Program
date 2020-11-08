52bbfc85bcd7f340b7b64e5cf63f77a305d89ea9
sandeep786 <sadeeprajsingh05@gmail.com>
2020-11-08 12:45:05 +0530

    ADDED: Feature to sort the list

---
+++ List/getList.py
@@ -0,0 +1,24 @@
+"""
+author -sandeep raj
+date -08-11-2020
+time -11:25
+package -List
+problem Statement-'
+Write a Python program to get a list, sorted in increasing order by the last
+element in each tuple from a given list of non-empty tuples.
+Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]'
+
+"""
+def last(n):
+    """
+
+    :param n:
+    :return:
+    """
+    return n[-1]
+
+def sort_list_last(tuples):
+  return sorted(tuples, key=last)
+
+print(sort_list_last([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
