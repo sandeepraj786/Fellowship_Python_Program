84bfe997926442b020e78accc88507035d3d0703
sandeep786 <sadeeprajsingh05@gmail.com>
2020-11-09 17:36:44 +0530

    ADDED: Feature to change upper case and lower case

---
+++ String/upperLowerCase.py
@@ -0,0 +1,13 @@
+"""
+author -sandeep raj
+date -09-11-2020
+time -17:35
+package -String
+problem Statement-'Write a Python script that takes input from the user and displays that input back in upper and lower cases
+"""
+
+
+
+user_input = input("Tell me your name: ")
+print("MY NAME IS  ", user_input.upper())
+print("My Name is  ", user_input.lower())
